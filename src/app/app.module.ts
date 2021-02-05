49b5fc739fbbc53a20e0fea43ba208cd678d1314
Jurfest <jurfest.apps@gmail.com>
2021-02-05 20:15:34 -0300

    creating main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { TabsComponent } from './components/authentication/tabs/tabs.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { RegisterComponent } from './components/authentication/register/register.component';
+import { LoggedInUserComponent } from './components/authentication/logged-in-user/logged-in-user.component';
+import { ErrorsComponent } from './components/shared/errors/errors.component';
+import { ChangeComponent } from './components/user/change/change.component';
+import { SearchComponent } from './components/user/search/search.component';
+import { DeleteComponent } from './components/user/delete/delete.component';
+import { ListComponent } from './components/user/list/list.component';
+import { NewComponent } from './components/user/new/new.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    LoginComponent,
+    RegisterComponent,
+    LoggedInUserComponent,
+    ErrorsComponent,
+    ChangeComponent,
+    SearchComponent,
+    DeleteComponent,
+    ListComponent,
+    NewComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
