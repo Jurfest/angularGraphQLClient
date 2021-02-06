7ae7411209cbdb7ef7a5b885e1b2e2c833d248f1
Jurfest <jurfest.apps@gmail.com>
2021-02-05 22:30:45 -0300

    adicionando botao em lista de usuarios

--- src/app/components/user/list/list.component.ts
+++ src/app/components/user/list/list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
-  selector: 'app-list',
+  selector: 'app-list-user',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  isLoading: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getUsers() {
+    // console.log(this.$api);
+    this.isLoading = true;
+    console.log("isLoading:", this.isLoading);
+    timer(2000).subscribe(() => {
+      this.isLoading = false;
+    });
+
   }
 
 }
